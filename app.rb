ad3ec58ad077a03d649e95d91676c5543a664bd7
Daniel M. Matongo <mmatongo_@hotmail.com>
2023-10-19 19:02:47 +0100

    update example

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require_relative 'lib/primrose'  # Adjust this path to where your Primrose library is located
 
 # Create a new store
-store = Primrose::Store.new({todos: ["Task 1", "Task 2"]})
+store = Primrose::Store.new({todos: [{text: "Task 1", completed: false}, {text: "Task 2", completed: false}]})
 
 helpers do
   include Primrose::Helpers
@@ -20,8 +20,16 @@ get '/' do
   erb :index
 end
 
+get '/about' do
+  @store.state.follow do |state|
+    @todos = state[:todos]
+  end
+
+  erb :about
+end
+
 post '/add_todo' do
-  new_todo = params[:new_todo]
+  new_todo = { text: params[:new_todo], completed: false }
   puts "Adding new todo: #{new_todo}"
   @store.dispatch(
     type: 'ADD_TODO',
