96ee0c45deaba12dda57b7ae48b08cb6c65a02ae
Daniel M. Matongo <mmatongo_@hotmail.com>
2023-10-20 18:41:53 +0100

    meh

--- lib/primrose/components/button.rb
+++ lib/primrose/components/button.rb
@@ -3,16 +3,15 @@ require_relative '../prim'
 module Primrose
   module Components
     class Button < Rose
-
-     TEMPLATE = <<~ERB
-       <button
-        onclick="<%= @action %>"
-        class="<%= @style %>"
-        <%= 'disabled' if @disabled %>
-        <%= 'loading' if @loading %>
-      >
-        <%= @label %>
-      </button>
+      TEMPLATE = <<~ERB
+        <button
+          onclick="<%= @action %>"
+          class="<%= @style %>"
+          <%= 'disabled' if @disabled %>
+          <%= 'loading' if @loading %>
+          >
+            <%= @label %>
+        </button>
       ERB
 
       def initialize(label:, action:, style: nil, disabled: false, loading: false)
