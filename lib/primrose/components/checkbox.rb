1cde865983cf744ee4b3b085f61a6353561257df
Daniel M. Matongo <mmatongo_@hotmail.com>
2023-10-20 13:44:49 +0100

    add js support

--- lib/primrose/components/checkbox.rb
+++ lib/primrose/components/checkbox.rb
@@ -3,11 +3,12 @@ require_relative '../prim'
 module Primrose
   module Components
     class Checkbox < Rose
-      def initialize(label:, checked: false, id: nil, name: nil)
+      def initialize(label:, checked: false, id: nil, name: nil, js: nil)
         @label = label
         @checked = checked
         @id = id || label.downcase.gsub(/\s+/, "_")  # Use label as id if id is not provided
         @name = name || @id  # Use id as name if name is not provided
+        @js = js
         super()
       end
 
